5d04b2d52a078936e3c1d8e3165d8b58bc71aaa7
Bad Cast <lmecomposer@gmail.com>
2023-05-10 16:33:18 +0600

    append check counter

--- shell-fetch_counter/check-counter.sh
+++ shell-fetch_counter/check-counter.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/sh
 
-$shell="shell-fetch_counter"
+shell="shell-fetch_counter"
 
-if [[ ! -e "./$shell" ]] then;
-    if [[ command -e cmake ]] && [[ command -e g++ ]] then
+if [[ ! -f "$(pwd)/${shell}" ]]; then
+    if [[ ! -f $(command -v cmake) ]] || [[ ! -f $(command -v g++) ]]; then
         echo "compiller is not installed"
         exit 1
     fi
+    tmp_dir="/tmp/${shell}_tmp/"
+    cmake -S "./${shell}-sources" -B ${tmp_dir} -DCMAKE_BUILD_TYPE=Release
+    cmake --build "${tmp_dir}" -j $(nproc)
+    cp -v ${tmp_dir}/$shell ./
+    rm -rf ${tmp_dir}
+    echo "compilling complete"
 fi
+
+chmod +x ./$shell
+"./$shell"
